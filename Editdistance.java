0ab3fdafeb4896fa0338c3bd1000ea2ebcdc3564
riyatyag <tyagiriya517@gmail.com>
2025-05-19 18:23:58 -0700

    Question

--- Editdistance.java
+++ Editdistance.java
@@ -1,3 +1,44 @@
-public class Editdistance {
-    
+// Problem Statement:
+// Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2. You have the following three operations permitted on a word: Insert a character, Delete a character, Replace a character.
+
+// Approach:
+// Dynamic Programming. Create a 2D array dp[i][j] representing the minimum number of operations to convert word1[0...i-1] to word2[0...j-1].
+
+// Time Complexity:
+// O(m * n), where m is the length of word1 and n is the length of word2.
+
+// Space Complexity:
+// O(m * n) for the dp array.
+
+// Optimal Solution:
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(
+                            dp[i - 1][j],
+                            Math.min(
+                                    dp[i][j - 1],
+                                    dp[i - 1][j - 1]
+                            )
+                    );
+                }
+            }
+        }
+        return dp[m][n];
+    }
 }
