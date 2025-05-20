4e519f2bbc7c2fd112b08319fcdab6358ba925bb
riyatyag <tyagiriya517@gmail.com>
2025-05-19 18:22:05 -0700

    Question

--- Interleavingstring.java
+++ Interleavingstring.java
@@ -1,3 +1,33 @@
-public class Interleavingstring {
-    
+// Problem Statement:
+// Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2. An interleaving of two strings s and t is a configuration where s and t are divided into n and m substrings respectively, such that: s = s1 + s2 + ... + sn, t = t1 + t2 + ... + tm, |n - m| <= 1. The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+
+// Approach:
+// Dynamic programming.  Use a 1D DP array to store whether s3[0...i+j-1] is an interleaving of s1[0...i-1] and s2[0...j-1].
+
+// Time Complexity:
+// O(m * n), where m is the length of s1 and n is the length of s2.
+
+// Space Complexity:
+// O(n), where n is the length of s2.
+
+// Optimal Solution:
+
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length(), l = s3.length();
+        if (m + n != l) return false;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int j = 1; j <= n; j++) {
+            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+        for (int i = 1; i <= m; i++) {
+            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) ||
+                        (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        return dp[n];
+    }
 }
