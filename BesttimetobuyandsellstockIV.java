ddaecc719fa1dce445b5cd2e6a3f045627154406
riyatyag <tyagiriya517@gmail.com>
2025-05-19 18:27:48 -0700

    Question

--- BesttimetobuyandsellstockIV.java
+++ BesttimetobuyandsellstockIV.java
@@ -1,3 +1,38 @@
-public class BesttimetobuyandsellstockIV {
-    
+// Problem Statement:
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k. Find the maximum profit you can achieve. You may complete at most k transactions: i.e. you may buy at most k times and sell at most k times.  Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+// Approach:
+// Dynamic Programming. dp[t][d] stores the maximum profit for at most t transactions up to day d.
+
+// Time Complexity:
+// O(k * n), where k is the maximum number of transactions and n is the number of days.
+
+// Space Complexity:
+// O(k * n) for the dp array.
+
+// Optimal Solution:
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        if (k >= n / 2) {
+            int maxProfit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1])
+                    maxProfit += prices[i] - prices[i - 1];
+            }
+            return maxProfit;
+        }
+
+        int[][] dp = new int[k + 1][n];
+        for (int t = 1; t <= k; t++) {
+            int maxDiff = -prices[0];
+            for (int d = 1; d < n; d++) {
+                dp[t][d] = Math.max(dp[t][d - 1], prices[d] + maxDiff);
+                maxDiff = Math.max(maxDiff, dp[t - 1][d] - prices[d]);
+            }
+        }
+        return dp[k][n - 1];
+    }
 }
