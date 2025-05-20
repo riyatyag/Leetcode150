8d9175bc1f8f5fb381de6ff3b40a118f166a0df4
riyatyag <tyagiriya517@gmail.com>
2025-05-19 18:17:15 -0700

    Question

--- UniquepathsII.java
+++ UniquepathsII.java
@@ -1,3 +1,41 @@
-public class UniquepathsII {
-    
+// Problem Statement:
+// You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time. An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle. Return the number of possible unique paths that the robot can take to reach the bottom-right corner. The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+// Approach:
+// Dynamic Programming.  Create a 2D dp array where dp[i][j] represents the number of unique paths to reach cell (i, j).
+
+// Time Complexity:
+// O(m * n), where m is the number of rows and n is the number of columns.
+
+// Space Complexity:
+// O(m * n) for the dp array.
+
+// Optimal Solution:
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
+            return 0;
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = (obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = (obstacleGrid[0][j] == 0 && dp[0][j - 1] == 1) ? 1 : 0;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 }
